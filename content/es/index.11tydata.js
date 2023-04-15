ee89aa742ee5522b7b57f1bdb52d58bde8aa6511
turinteras <turinteras@proton.me>
2023-04-15 15:58:36 +0200

    fix incorrect links

--- content/es/index.11tydata.js
+++ content/es/index.11tydata.js
@@ -2,8 +2,8 @@ module.exports = {
 	layout: "layouts/base.njk",
 	eleventyComputed: {
 		eleventyNavigation: {
-			key: (data) => data?.lang[data?.page?.lang]?.navContact,
-			order: 3,
+			key: (data) => data?.lang[data?.page?.lang]?.navHome,
+			order: 1,
 		},
 	},
 };
